ee6355f58e656129ad2f1bfb07e08cfe3320ec65
Ashleigh Carter <ashbcarter@gmail.com>
2024-02-22 18:30:05 -0500

    added base saga functions for get axios request. Need to set up reducers and add in missing pieces of code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,12 @@
 import Search from '../Search/Search';
 import Favorites from '../Favorites/Favorites';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const getCategory = () => {
+    dispatch({ type: 'GET_' });
+  };
+
   return (
     <div>
       <h1>Giphy Search!</h1>
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,7 @@
-import { createStore, combineReducer, applyMiddlewar } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
 
 //REDUCER(redux)
 
@@ -8,18 +9,29 @@ import createSagaMiddleware from 'redux-saga';
 const sagaMiddleware = createSagaMiddleware();
 
 //SAGA GENERATOR FUNCTIONS
+function firstSaga(action) {
+  const categoryResponse = yield axios({
+    method: 'GET',
+    url:'/',
+  });
+//dispatch to TYPE FROM REDUCER with categoryResponse.data
+yield put({ type: '', payload: categoryResponse.data })
+}
+
 
 //CORE SAGA function(generator function), ALL SAGA FUNCTIONS WILL GET PASSED HERE
 //CAN BE CALLED ANYTHING
 function* watcherSaga() {
   //NEED YIELD
+  //need type from reducer in the takeEvery string ('')
+  yield takeEvery('GET_', firstSaga)
   const response = yield axios.get();
 }
 
 //STORES REDUCERS
 const store = createStore(
-  combineReducer({}),
-  applyMiddlewar(sagaMiddleware, logger)
+  combineReducers({}),
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 //CORE SAGA FUNCTION GETS PASSED HERE
