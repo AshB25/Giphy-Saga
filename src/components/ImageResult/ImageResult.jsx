3d860d51322694ade7779e045f5ddb3c8f9ca616
Cali Knowlden <calbearnc@gmail.com>
2024-02-25 23:29:02 -0600

    added styling to like button

--- src/components/ImageResult/ImageResult.jsx
+++ src/components/ImageResult/ImageResult.jsx
@@ -1,17 +1,32 @@
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import FavButton from '../FavButton/FavButton';
+import { FcLikePlaceholder, FcLike } from 'react-icons/fc';
 
 function ImageResult() {
   const giphy = useSelector((store) => store.giphy);
+  const [fav, setFav] = useState(false);
 
+  const handleClick = () => {
+    setFav(!fav);
+    if (fav) {
+      return <FcLikePlaceholder size="35" onClick={handleClick} />;
+    }
+    // return <FcLike size="35" onClick={handleClick} />;
+  };
   return (
     <div className="img-group">
       <ul>
         {giphy.map((giphyImage) => {
           return (
-            <span className="cardColor">
-              <img src={giphyImage.url} alt={giphyImage.alt} />
-              <button>Like</button>
-            </span>
+            <>
+              <span className="cardColor">
+                <img src={giphyImage.url} alt={giphyImage.alt} />
+                <button className="like-btn">
+                  {fav ? <FcLikePlaceholder /> : <FcLike />}
+                </button>
+              </span>
+            </>
           );
         })}
       </ul>
